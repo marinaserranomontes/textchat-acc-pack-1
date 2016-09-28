732372eb4af6ca0ed6971d795c1588f914b28ea8
marinas <marinaserranomontes@gmail.com>
2016-09-28 16:15:49 +0200

    Removing credentials

--- android/TextChatAccPackKit/text-chat-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/textchat/config/APIConfig.java
+++ android/TextChatAccPackKit/text-chat-acc-pack-kit/src/androidTest/java/com/tokbox/android/accpack/textchat/config/APIConfig.java
@@ -3,9 +3,9 @@ package com.tokbox.android.accpack.textchat.config;
 
 public class APIConfig {
 
-    public static int API_KEY = 45621172; // Fill this in with generated API Key (http://tokbox.com/opentok/api/tools/js/apikey)
-    public static String TOKEN = "T1==cGFydG5lcl9pZD00NTYyMTE3MiZzZGtfdmVyc2lvbj10YnBocC12MC45MS4yMDExLTA3LTA1JnNpZz0xZTU0NzVmNGU5MGU1MzUyYWZmNzA2NzI3ODQ0MTM2ZjYyMDY0MWIxOnNlc3Npb25faWQ9MV9NWDQwTlRZeU1URTNNbjUtTVRRMk9EUXdOemd4TmpZNE5YNUNhRlJMVjJKNVJEWlVVWEJaZWsxeFRWVm9aRXR5UTI1LWZnJmNyZWF0ZV90aW1lPTE0NzQ0NjkxNTMmcm9sZT1tb2RlcmF0b3Imbm9uY2U9MTQ3NDQ2OTE1My4xMzQ2MTk3MDg0NDI0NiZleHBpcmVfdGltZT0xNDc3MDYxMTUz";
-    public static String SESSION_ID = "1_MX40NTYyMTE3Mn5-MTQ2ODQwNzgxNjY4NX5CaFRLV2J5RDZUUXBZek1xTVVoZEtyQ25-fg";
+    public static String API_KEY = ""; // Fill this in with generated API Key (http://tokbox.com/opentok/api/tools/js/apikey)
+    public static String TOKEN = "";
+    public static String SESSION_ID = "";
 
 
 }
